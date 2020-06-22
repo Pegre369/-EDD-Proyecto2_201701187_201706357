17dcb36c9e268ef2f6359a49ae1225b6f339751b
Pedro <pedro.ordoniez@gmail.com>
2020-06-21 22:04:44 -0600

    Prueba

---
+++ [EDD]Proyecto2/src/Nodos/Vertice.java
@@ -0,0 +1,19 @@
+package Nodos;
+
+/**
+ *
+ * @author Casca
+ */
+public class Vertice {
+    
+    public Vertice Siguiente;
+    private String Ciudad;
+
+    public Vertice(String Ciudad) {
+        this.Ciudad = Ciudad;
+        this.Siguiente = null;
+    }
+    
+    
+    
+}
