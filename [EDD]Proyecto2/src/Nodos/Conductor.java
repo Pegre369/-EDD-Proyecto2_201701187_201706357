939869f0755c8ac5a9d9153cd3ee22c7c6738dae
Pedro <pedro.ordoniez@gmail.com>
2020-06-25 22:23:43 -0600

    11

--- [EDD]Proyecto2/src/Nodos/Conductor.java
+++ [EDD]Proyecto2/src/Nodos/Conductor.java
@@ -6,6 +6,8 @@ import java.math.*;
  */
 public class Conductor {
     
+    public Conductor Siguiente;
+    public Conductor Anterior;
     private BigInteger Dpi;
     private String Nombre;
     private String Apellido;
@@ -16,6 +18,8 @@ public class Conductor {
     private String Direccion;
 
     public Conductor(BigInteger Dpi, String Nombre, String Apellido, String Licencia, String Genero, String FechaNac, int Telefono, String Direccion) {
+        this.Siguiente = null;
+        this.Anterior = null;
         this.Dpi = Dpi;
         this.Nombre = Nombre;
         this.Apellido = Apellido;
---
+++ [EDD]Proyecto2/src/edd/proyecto2/Seleccionar_Archivo_Ruta.java
@@ -0,0 +1,103 @@
+
+package edd.proyecto2;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Casca
+ */
+public class Seleccionar_Archivo_Ruta extends javax.swing.JFrame {
+
+   public static String ruta;
+   
+    public Seleccionar_Archivo_Ruta() {
+        initComponents();
+    }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jFileChooser1 = new javax.swing.JFileChooser();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jFileChooser1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jFileChooser1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jFileChooser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFileChooser1ActionPerformed
+
+        JFileChooser SelectorArchivo = (JFileChooser) evt.getSource();
+        String command = evt.getActionCommand();
+        if (command.equals(JFileChooser.APPROVE_SELECTION)) {
+            File archivoSeleccionado = SelectorArchivo.getSelectedFile();
+            System.out.println(archivoSeleccionado.getAbsolutePath());
+            ruta=archivoSeleccionado.getAbsolutePath();
+           Cargar_Rutas.begin();
+            hide();
+        } else if (command.equals(JFileChooser.CANCEL_SELECTION)) {
+            //JOptionPane.showMessageDialog(this, "Selecciona un Archivo...");
+            hide();
+        }
+
+    }//GEN-LAST:event_jFileChooser1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Seleccionar_Archivo_Ruta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Seleccionar_Archivo_Ruta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Seleccionar_Archivo_Ruta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Seleccionar_Archivo_Ruta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Seleccionar_Archivo_Ruta().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JFileChooser jFileChooser1;
+    // End of variables declaration//GEN-END:variables
+}
